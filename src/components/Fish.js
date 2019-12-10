e9c1ab7935286b1f7221eb3b1ec5643a36cdf809
ragith <ragith.coolkid@gmail.com>
2019-12-10 11:53:38 -0500

    initial commit

---
+++ src/components/Fish.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Fish extends React.Component {
+  render() {
+    return <div className="single-fish">
+    	FISH
+    </div>;
+  }
+}
+
+export default Fish;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker';
+import App from './components/App.js';
+import './css/style.css';
+
+render(<App />, document.querySelector('#main'));
